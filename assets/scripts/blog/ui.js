2de6cf132da8cb912947954ed63b423abdc3aa8a
freef <mtfreeland@gmail.com>
2019-03-27 20:20:26 -0400

    add events to clear forms on submission
    used input and trigger reset to clear forms after a form is submitted

--- assets/scripts/blog/ui.js
+++ assets/scripts/blog/ui.js
@@ -5,6 +5,7 @@ const onCreateBlogSuccess = (responseData) => {
   $('#user-message').text('successfully created post!')
   const showBlogHtml = showBlogTemplate({ blog: responseData.blog })
   $('input').trigger('reset')
+  $('form').trigger('reset')
   $('#blog-content').empty()
   $('#blog-content').append(showBlogHtml)
 }
@@ -12,24 +13,32 @@ const onCreateBlogSuccess = (responseData) => {
 const onIndexBlogSuccess = (responseData) => {
   $('#user-message').text('successfully got posts!')
   const showBlogHtml = showBlogTemplate({ blog: responseData.blog })
+  $('form').trigger('reset')
   $('input').trigger('reset')
   $('#blog-content').empty()
   $('#blog-content').append(showBlogHtml)
 }
 const onShowBlogSuccess = (responseData) => {
   const showBlogHtml = showBlogTemplate({ blog: responseData })
+  $('form').trigger('reset')
   $('input').trigger('reset')
   $('#blog-content').empty()
   $('#blog-content').append(showBlogHtml)
 }
 const onUpdateBlogSuccess = (responseData) => {
+  $('input').trigger('reset')
+  $('form').trigger('reset')
   $('#user-message').text('successfully updated post!')
 }
 const onDeleteBlogSuccess = (responseData) => {
+  $('input').trigger('reset')
+  $('form').trigger('reset')
   $('#user-message').text('successfully deleted post!')
 }
 
 const onBlogFailure = (responseData) => {
+  $('input').trigger('reset')
+  $('form').trigger('reset')
   $('#user-message').text('something went wrong...')
 }
 
