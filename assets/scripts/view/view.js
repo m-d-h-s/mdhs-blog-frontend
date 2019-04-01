4083ea53730869a24809156b934c9c00accad85e
davidholyko <david.holy.ko@gmail.com>
2019-03-31 21:31:31 -0400

    add editable

--- assets/scripts/view/view.js
+++ assets/scripts/view/view.js
@@ -3,6 +3,7 @@ const store = require('../store')
 const showOwnership = () => {
   if (!store.user) { return }
   $(`.owned[data-owner=${store.user._id}]`).show()
+  $(`.editable[data-owner=${store.user._id}]`).text('editable')
 }
 
 const hideOwnership = () => { $('.owned').hide() }
